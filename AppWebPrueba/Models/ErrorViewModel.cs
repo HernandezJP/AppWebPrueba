ac0e1724184712b4298628b66ec85cb463db2360
Jose Pablo Hernandez Raymundo <josepablohr@outlook.es>
2025-10-27 00:45:56 -0600

    Agregar archivos de proyecto.

---
+++ AppWebPrueba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppWebPrueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
